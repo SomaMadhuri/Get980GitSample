923f392300672c0beea2d3014deb5944eb6953c5
SomaMadhuri <madhusoma125@gmail.com>
2021-12-20 17:45:38 +0530

    First Commit

---
+++ src/main/java/com/lti/Calculate.java
@@ -0,0 +1,12 @@
+package com.lti;
+
+public class Calculate {
+	
+	public static int calculate(int x, int y) {
+		return x+y;
+	}
+	public static void main(String[] args) {
+		
+		calculate(10,5);
+		}
+	}
